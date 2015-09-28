cee1cb3644b62a52502c67d382c845678b82deec
Povilas Simanaitis <povilozauras@gmail.com>
2015-09-29 00:35:56 +0300

    Working draft, will be fixed later.

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -1,3 +1,7 @@
+Games = new Mongo.Collection("games");
+
+
+
 if (Meteor.isClient) {
   angular.module('tictactoe',['angular-meteor', 'ui.router']);
 
@@ -36,51 +40,65 @@ if (Meteor.isClient) {
 
     }]);
 
-  angular.module('tictactoe').controller('tictactoeCtrl', ['$scope', '$stateParams',
-    function ($scope, $stateParams) {
+  angular.module('tictactoe').controller('tictactoeCtrl', ['$scope', '$stateParams', '$meteor',
+    function ($scope, $stateParams, $meteor) {
 
-      $scope.gameId = $stateParams.gameId;
+      $scope.games = $meteor.collection(Games);
 
-      $scope.endMessage = false;
-      $scope.fields = [];
-      $scope.currentPlayer = "X";
-
-      $scope.init = function(){
-        for(var i = 0; i < 9; i++){
-          $scope.fields[i] = {"player":" "};
-        }
-        $scope.endMessage = false;
+      function Game(size, winRow){
+        this.size = size;
+        this.winRow = winRow;
+        this.endMessage = false;
+        this.fields = [];
+        this.currentPlayer = "X";
       };
 
-      $scope.init();
+      Game.prototype.init = function(){
+        for(var i = 0; i < this.size*this.size; i++){
+          this.fields[i] = {"player":" "};
+        }
+      };
+
+      $scope.init = function(){
+       if($stateParams.gameId > -1){
+          $scope.game = $scope.games[$stateParams.gameId];
+          $scope.currentGameNumber = $stateParams.gameId;
+        }else{
+          $scope.game =  new Game(3,3);
+          $scope.game.init();
+          $scope.currentGameNumber = $scope.games.length;
+          $scope.games.save($scope.game);
+        }
+      };
 
       $scope.occupieField = function(field){
         if(field.player !== ' ') return true;
-        field.player = $scope.currentPlayer;
+        field.player = $scope.game.currentPlayer;
         _checkIfWinnerExist();
 
-        $scope.currentPlayer = ("X" == $scope.currentPlayer) ? "O" : "X";
-        if(_countEmptyPlace() === 0) $scope.endMessage = 'There is no winners its a draw.';
+        $scope.game.currentPlayer = ("X" == $scope.game.currentPlayer) ? "O" : "X";
+        if(_countEmptyPlace() === 0) $scope.game.endMessage = 'There is no winners its a draw.';
+        $scope.games[$scope.currentGameNumber] = $scope.game;
       };
 
       var _countEmptyPlace = function (){
-        return $scope.fields.filter(function(value){
+        return $scope.game.fields.filter(function(value){
           return value.player === ' ';
         }).length;
       };
 
       var _printWinner = function(){
-        $scope.endMessage = 'Congratulations winner is player ' + $scope.currentPlayer + '.';
+        $scope.game.endMessage = 'Congratulations winner is player ' + $scope.game.currentPlayer + '.';
       };
 
       var _checkIfWinnerExist = function _checkIfWinnerExist(){
         for (var i =0; i <3; i++){
-          if( $scope.fields[0+i*3].player == $scope.fields[1+i*3].player && $scope.fields[1+i*3].player == $scope.fields[2+i*3].player && $scope.fields[0+i*3].player != " " ) _printWinner();
-          if( $scope.fields[0+i].player == $scope.fields[3+i].player && $scope.fields[3+i].player ==  $scope.fields[6+i].player && $scope.fields[0+i].player!= " ") _printWinner();
+          if( $scope.game.fields[0+i*3].player == $scope.game.fields[1+i*3].player && $scope.game.fields[1+i*3].player == $scope.game.fields[2+i*3].player && $scope.game.fields[0+i*3].player != " " ) _printWinner();
+          if( $scope.game.fields[0+i].player == $scope.game.fields[3+i].player && $scope.game.fields[3+i].player ==  $scope.game.fields[6+i].player && $scope.game.fields[0+i].player!= " ") _printWinner();
         }
 
-        if($scope.fields[0].player == $scope.fields[4].player && $scope.fields[4].player  ==  $scope.fields[8].player && $scope.fields[4].player!= " ") _printWinner();
-        if($scope.fields[2].player == $scope.fields[4].player && $scope.fields[4].player  ==  $scope.fields[6].player && $scope.fields[4].player!= " ") _printWinner();
+        if($scope.game.fields[0].player == $scope.game.fields[4].player && $scope.game.fields[4].player  ==  $scope.game.fields[8].player && $scope.game.fields[4].player!= " ") _printWinner();
+        if($scope.game.fields[2].player == $scope.game.fields[4].player && $scope.game.fields[4].player  ==  $scope.game.fields[6].player && $scope.game.fields[4].player!= " ") _printWinner();
         };
 
     }]);
