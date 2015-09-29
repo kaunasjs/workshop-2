2e97caa751bb770f97e9807dc93110b02b500076
Povilas Simanaitis <povilozauras@gmail.com>
2015-09-29 21:30:04 +0300

    Add second player and minor fixes.

--- tictactoe/app.js
+++ tictactoe/app.js
@@ -2,13 +2,13 @@ Games = new Mongo.Collection("games");
 
 Games.allow({
   insert: function (userId, game) {
-    return userId && game.owner === userId;
+    return userId && (game.owner === userId || game.concurent === userId || !game.concurent);
   },
   update: function (userId, game) {
-    return userId && game.owner === userId;
+    return userId && (game.owner === userId || game.concurent === userId || !game.concurent);
   },
   remove: function (userId, game) {
-    return userId && game.owner === userId;
+    return userId && (game.owner === userId || game.concurent === userId || !game.concurent);
   }
 });
 
@@ -17,14 +17,11 @@ if (Meteor.isClient) {
 
   angular.module("tictactoe").run(['$rootScope', '$state', function($rootScope, $state) {
     $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-      // We can catch the error thrown when the $requireUser promise is rejected
-      // and redirect the user back to the main page
       if (error === 'AUTH_REQUIRED') {
         $state.go('main');
       }
     });
   }]);
-
 
   angular.module('tictactoe').config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
     function($urlRouterProvider, $stateProvider, $locationProvider){
@@ -36,16 +33,6 @@ if (Meteor.isClient) {
             url: '/main',
             templateUrl: 'index.ng.html',
             controller: 'mainCtrl'
-          })
-          .state('game-list', {
-            url: '/game-list/',
-            templateUrl: 'party-details.ng.html',
-            controller: 'gameListCtrl',
-            resolve: {
-              "currentUser": ["$meteor", function($meteor){
-                return $meteor.requireUser();
-              }]
-            }
           })
           .state('game', {
             url: '/game/:gameId',
@@ -61,14 +48,9 @@ if (Meteor.isClient) {
       $urlRouterProvider.otherwise("/main");
     }]);
 
-  angular.module('tictactoe').controller('gameListCtrl', ['$scope',
-    function ($scope) {
-
-    }]);
-
-  angular.module('tictactoe').controller('mainCtrl', ['$scope',
-    function ($scope) {
-
+  angular.module('tictactoe').controller('mainCtrl', ['$scope', '$meteor',
+    function ($scope, $meteor) {
+      $scope.gameList = $meteor.collection(Games);
     }]);
 
   angular.module('tictactoe').controller('tictactoeCtrl', ['$scope', '$stateParams', '$meteor', '$rootScope',
@@ -92,9 +74,14 @@ if (Meteor.isClient) {
       };
 
       $scope.init = function(){
-       if($stateParams.gameId > -1){
+
+       if($stateParams.gameId > -1 && $scope.games[$stateParams.gameId]){
           $scope.game = $scope.games[$stateParams.gameId];
           $scope.currentGameNumber = $stateParams.gameId;
+          if($rootScope.currentUser._id != $scope.game.owner && !$scope.game.concurent){
+            $scope.game.concurent = $rootScope.currentUser._id;
+            $scope.games[$scope.currentGameNumber] = $scope.game;
+          }
         }else{
           $scope.game =  new Game(3,3);
           $scope.game.init();
@@ -128,7 +115,6 @@ if (Meteor.isClient) {
           if( $scope.game.fields[0+i*3].player == $scope.game.fields[1+i*3].player && $scope.game.fields[1+i*3].player == $scope.game.fields[2+i*3].player && $scope.game.fields[0+i*3].player != " " ) _printWinner();
           if( $scope.game.fields[0+i].player == $scope.game.fields[3+i].player && $scope.game.fields[3+i].player ==  $scope.game.fields[6+i].player && $scope.game.fields[0+i].player!= " ") _printWinner();
         }
-
         if($scope.game.fields[0].player == $scope.game.fields[4].player && $scope.game.fields[4].player  ==  $scope.game.fields[8].player && $scope.game.fields[4].player!= " ") _printWinner();
         if($scope.game.fields[2].player == $scope.game.fields[4].player && $scope.game.fields[4].player  ==  $scope.game.fields[6].player && $scope.game.fields[4].player!= " ") _printWinner();
         };
